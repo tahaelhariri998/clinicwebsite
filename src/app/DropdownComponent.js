3ac81c1995b2d596c3bb917653625b4118ae7baf
Taha elhariri <tahaalhriri@gmail.com>
2024-12-11 21:52:31 +0300

    Initial commit message  dfdsgcvfdsfdssfsfسبسgfgfبسبسبسdسبسبسxv

--- src/app/DropdownComponent.js
+++ src/app/DropdownComponent.js
@@ -9,11 +9,12 @@ const DropdownComponent = ({ headerSvg, contentSvg }) => {
         border: "1px solid #e0e0e0",
         borderRadius: "8px",
         padding: "16px",
-        width: "100%",
-        maxWidth: "400px",
+        width: "90%",
+        
         fontFamily: "Arial, sans-serif",
         direction: "rtl",
         textAlign: "right",
+        fontSize: "150%",
         background: "#fff",
       }}
     >
@@ -31,7 +32,7 @@ const DropdownComponent = ({ headerSvg, contentSvg }) => {
           {headerSvg}
         </div>
         <span style={{ transform: isOpen ? "rotate(0deg)" : "rotate(90deg)" }}>
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg className="w-full h-auto" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M5 9L11.2191 14.3306C11.6684 14.7158 12.3316 14.7158 12.7809 14.3306L19 9" stroke="#717171" stroke-width="1.5" stroke-linecap="round"/>
 </svg>
 
@@ -43,7 +44,7 @@ const DropdownComponent = ({ headerSvg, contentSvg }) => {
       {isOpen && (
         
         <div style={{ marginTop: "10px" }}>
-            <svg width="337" height="2" viewBox="0 0 337 2" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg className="w-full h-auto"  width="337" height="2" viewBox="0 0 337 2" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M337 1H0" stroke="#E6E6E6"/>
 </svg>
 
